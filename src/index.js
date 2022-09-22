4efd7078c50c5faead5f3a44ccaca3b3992eb956
peeyush1328 <peeyushkumar890@gmail.com>
2022-09-22 14:20:06 +0530

    Added Scrolltotop Componenet

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import  Moviecontex  from "./Contex/Moviecontex";
+import ScrollToTop from "./Hoc/Scrolltotop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Moviecontex>
+      <ScrollToTop />
         <App />
       </Moviecontex>
     </BrowserRouter>
